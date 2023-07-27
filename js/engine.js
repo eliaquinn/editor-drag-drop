0bd41c8e5f66fa982e49abf965342d7b95c7e7f7
Eliaquin Araujo <eliaquin.j.araujo>
2023-07-26 21:12:07 -0400

    added some js libs for testing purposes

--- js/engine.js
+++ js/engine.js
@@ -1,24 +1,5 @@
-class GridStackBuilder extends GridStackEngine {
+class DocumentEngine {
     constructor(arr) {
         this.object = arr
-    }
-
-    createElement (arr, element, index) {
-        const {column, row, cellHeight, disableOneColumnMode ,float, removable} = this.object[index]
-        GridStack.init({
-            column,
-            row,
-            cellHeight,
-            disableOneColumnMode,
-            float,
-            removable,
-        }, element)
-        .load(arr)
-    }
-
-    moveNode(GridStackNode, GridStackMoveOpts) {
-        o.x = node.x;
-        o.w = node.w;
-        return super.moveNode(node, o);
     }
 }
